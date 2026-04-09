2a0bc698073b93d6198a897522bad01f4d2a2922
jurisbogd <juris.bogdanas@gmail.com>
2026-04-09 22:47:18 +0300

    Converted Draw to ts

--- packages/gameclient/src/Draw.ts
+++ packages/gameclient/src/Draw.ts
@@ -1,5 +1,6 @@
 import { Rect } from "./math/Rect.js";
 import { Vec2 } from "./math/Vec2.js";
+import { Spritesheet } from "./Spritesheet.js";
 
 export class Draw {
     sprite;
@@ -8,14 +9,14 @@ export class Draw {
     pivot;
     depth = 0;
 
-    constructor(sprite, transform, sprite_rect, pivot) {
+    constructor(sprite: any, transform: Rect, sprite_rect: Rect, pivot: Vec2) {
         this.sprite = sprite;
         this.transform = transform;
         this.sprite_rect = sprite_rect;
         this.pivot = pivot;
     }
 
-    static image(image, x, y) {
+    static image(image: any, x: number, y: number): Draw {
         const transform = new Rect(x, y, image.width, image.height);
         const sprite_rect = new Rect(0, 0, image.width, image.height);
         const pivot = Vec2.zero();
@@ -25,7 +26,7 @@ export class Draw {
         return draw;
     }
 
-    static sprite(spritesheet, id, x, y) {
+    static sprite(spritesheet: Spritesheet, id: string, x: number, y: number) {
         const sprite_rect = Rect.copy(spritesheet.get_sprite_rect(id));
         const transform = new Rect(x, y, sprite_rect.get_width(), sprite_rect.get_height());
         const sprite_pivot = spritesheet.get_sprite_pivot(id);
@@ -36,13 +37,13 @@ export class Draw {
         return draw;
     }
 
-    set_position(x, y) {
+    set_position(x: number, y: number) {
         this.transform.set_position(x, y);
 
         return this;
     }
 
-    set_scale(x, y) {
+    set_scale(x: number, y: number) {
         this.transform.set_size(
             this.sprite_rect.get_width() * x,
             this.sprite_rect.get_height() * y
@@ -51,33 +52,33 @@ export class Draw {
         return this;
     }
 
-    set_scale_absolute(x, y) {
+    set_scale_absolute(x: number, y: number) {
         this.transform.set_size(x, y);
 
         return this;
     }
 
-    set_pivot(x, y) {
+    set_pivot(x: number, y: number) {
         this.pivot.set_x(x * this.transform.get_width());
         this.pivot.set_y(y * this.transform.get_height());
 
         return this;
     }
 
-    set_pivot_absolute(x, y) {
+    set_pivot_absolute(x: number, y: number) {
         this.pivot.set_x(x);
         this.pivot.set_y(y);
 
         return this;
     }
 
-    set_depth(depth) {
+    set_depth(depth: number) {
         this.depth = depth;
 
         return this;
     }
 
-    get_top() {
+    get_top(): number {
         const y = this.transform.get_y();
         const pivot_y = this.pivot.get_y();
         const top = y - pivot_y;
@@ -85,7 +86,7 @@ export class Draw {
         return top;
     }
 
-    get_bottom() {
+    get_bottom(): number {
         const y = this.transform.get_y();
         const height = this.transform.get_height();
         const pivot_y = this.pivot.get_y();
@@ -94,7 +95,7 @@ export class Draw {
         return bottom;
     }
 
-    get_left() {
+    get_left(): number {
         const x = this.transform.get_x();
         const pivot_x = this.pivot.get_x();
         const left = x - pivot_x;
@@ -102,7 +103,7 @@ export class Draw {
         return left;
     }
 
-    get_right() {
+    get_right(): number {
         const x = this.transform.get_x();
         const width = this.transform.get_width();
         const pivot_x = this.pivot.get_x();
@@ -112,7 +113,7 @@ export class Draw {
     }
 
     // set depth to bottom of rendered sprite
-    set_depth_bottom(depth_mod = 0) {
+    set_depth_bottom(depth_mod: number = 0) {
         const bottom = this.get_bottom();
         const depth = bottom + depth_mod;
         this.depth = depth;
