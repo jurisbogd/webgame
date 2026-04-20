e4a8bd145d9122ce567e1121b85f45b330d6e226
jurisbogd <juris.bogdanas@gmail.com>
2026-04-20 23:57:08 +0300

    Removed deprecated packages

--- packages/gameserver/src/math/cantor_pair.js
+++
@@ -1,3 +0,0 @@
-export function cantor_pair(a, b) {
-    return (a + b) * (a + b + 1) / 2 + b
-}
--- packages/gameserver/src/select_random.js
+++
@@ -1,4 +0,0 @@
-export function select_random(array) {
-    const index = Math.floor(Math.random() * array.length)
-    return array[index]
-}
